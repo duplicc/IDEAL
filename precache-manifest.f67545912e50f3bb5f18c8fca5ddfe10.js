0733a63b6c3cb7bbfb512e5a18e54efc14cbfb86
duplicc <duplicc@nuaa.edu.cn>
2020-06-20 22:01:14 +0800

    test gh-pages

---
+++ precache-manifest.f67545912e50f3bb5f18c8fca5ddfe10.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "440dfa4e5fa10d1e7cb7",
+    "url": "./static/js/main.440dfa4e.chunk.js"
+  },
+  {
+    "revision": "75cb43014aa7c52562fe",
+    "url": "./static/css/1.c7146e87.chunk.css"
+  },
+  {
+    "revision": "75cb43014aa7c52562fe",
+    "url": "./static/js/1.75cb4301.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "76f2c161cdc16a015cdbd4425733cd5e",
+    "url": "./static/media/welcome-inc.76f2c161.png"
+  },
+  {
+    "revision": "674f794b529b7e951bf8a156162dbadd",
+    "url": "./static/media/sys-rank.674f794b.png"
+  },
+  {
+    "revision": "affe3666ee73e980c046e1eabe85f113",
+    "url": "./static/media/result-inc.affe3666.png"
+  },
+  {
+    "revision": "e1976470d6a1007f4002e70f5619ab7b",
+    "url": "./index.html"
+  }
+];
