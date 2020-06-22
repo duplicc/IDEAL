368f4e893a50218fd0b04cad2aef5fb0cf8b6c30
duplicc <duplicc@nuaa.edu.cn>
2020-06-22 21:58:23 +0800

    test gh-pages

---
+++ build/precache-manifest.a1694a69667bd54d68cd2085c63aa80d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a58831cfeb07118b3b7",
+    "url": "./static/js/main.1a58831c.chunk.js"
+  },
+  {
+    "revision": "75cb43014aa7c52562fe",
+    "url": "./static/css/1.c7146e87.chunk.css"
+  },
+  {
+    "revision": "75cb43014aa7c52562fe",
+    "url": "./static/js/1.75cb4301.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "./static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "./static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "./static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "./static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "./static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e3868d0b37536be4c217a09d7c937ae0",
+    "url": "./static/media/welcome-inc.e3868d0b.png"
+  },
+  {
+    "revision": "2a0d2ab760f150eb6d7e5313300c9e93",
+    "url": "./static/media/sorting-inc.2a0d2ab7.png"
+  },
+  {
+    "revision": "b60e6a6ebff512bd681f79b90d3aed18",
+    "url": "./static/media/result-inc.b60e6a6e.png"
+  },
+  {
+    "revision": "c130a28eca8c3c2cec96ebe42542a477",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.dcc4a7cb042e071eefff2751d6ddbde1.js"
+  "./precache-manifest.a1694a69667bd54d68cd2085c63aa80d.js"
 );
 
 workbox.clientsClaim();
