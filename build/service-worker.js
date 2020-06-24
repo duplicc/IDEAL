062ec39e7fd03983809a7831227fa084e8043a12
duplicc <duplicc@nuaa.edu.cn>
2020-06-24 21:16:34 +0800

    test gh-pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.fe6683fa3d6f001aac6a29a225328c44.js"
+  "./precache-manifest.bda12241e4af12c5344bae76f32a7fd0.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
